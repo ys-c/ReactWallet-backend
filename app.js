581b9e577e86659e2797e83bede35e0173d72bc8
ys-c <ys.chiaw@gmail.com>
2024-04-02 02:31:13 +0800

    removed redundant code and added cors dependency

--- app.js
+++ app.js
@@ -7,17 +7,19 @@ const bodyPraser = require("body-parser");
 
 // set up express app
 const app = express();
+var cors = require('cors');
+app.use(cors());
 
 // utilize body parser
 app.use(bodyPraser.json());
 // setting up routes
  app.use(require("./routes"));
 
-app.get('/home', function(req,res){
-    console.log('here');
-});
+// app.get('/home', function(req,res){
+//     console.log('here');
+// });
 
 //listen for request
-app.listen(process.env.port || 3000, function(){
+app.listen(process.env.port || 4000, function(){
     console.log('now listening to request');
 });
